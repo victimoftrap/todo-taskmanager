de88ac9a2c3bf59aa925960dcfb41b55d38af318
victimoftrap <monsquizo@gmail.com>
2019-04-10 19:43:32 +0600

    Added handling of NumberFormatException

---
+++ src/main/java/it/sevenbits/backend/taskmanager/web/service/RestExceptionHandler.java
@@ -0,0 +1,27 @@
+package it.sevenbits.backend.taskmanager.web.service;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Class that handles exceptions from service
+ */
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    /**
+     * Handle exception about a non-integer value of page/size
+     *
+     * @param ex exception of number format
+     * @return ResponseEntity with message
+     */
+    @ExceptionHandler(NumberFormatException.class)
+    protected ResponseEntity<Object> handleNumberFormatException(final NumberFormatException ex) {
+        return ResponseEntity
+                .badRequest()
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .build();
+    }
+}
